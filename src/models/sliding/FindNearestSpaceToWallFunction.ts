800b977b28b00f1acb0d2a9bcd39087d4d64f8f0
Craig McNicholas <craig.mcnicholas@yotta.co.uk>
2023-10-23 01:22:49 +0100

    feat: algorithm to find nearest empty space in a direction
    each implementation is specific

---
+++ src/models/sliding/FindNearestSpaceToWallFunction.ts
@@ -0,0 +1,67 @@
+import type { Matrix } from "../Matrix";
+
+export type FindNearestSpaceToWallFunction = (
+  matrix: Matrix,
+  columnOrRow: number,
+  currentPosition: number
+) => number | undefined;
+
+export const nearestSpaceToWallAbove: FindNearestSpaceToWallFunction = (
+  matrix: Matrix,
+  column: number,
+  currentPosition: number,
+): number | undefined => {
+  // start from current position - 1 to find closest empty space
+  let index: number | undefined;
+  for (let z = currentPosition - 1; z >= 0; z--) {
+    if (matrix.getSquare(column, z) === undefined) {
+      index = z;
+    }
+  }
+  return index;
+};
+
+export const nearestSpaceToWallBelow: FindNearestSpaceToWallFunction = (
+  matrix: Matrix,
+  column: number,
+  currentPosition: number,
+): number | undefined => {
+  // start from current position + 1 to find closest empty space
+  let index: number | undefined;
+  for (let z = currentPosition + 1; z < matrix.size; z++) {
+    if (matrix.getSquare(column, z) === undefined) {
+      index = z;
+    }
+  }
+  return index;
+};
+
+export const nearestSpaceToWallLeft: FindNearestSpaceToWallFunction = (
+  matrix: Matrix,
+  row: number,
+  currentPosition: number,
+): number | undefined => {
+  // start from current position - 1 to find closest empty space
+  let index: number | undefined;
+  for (let z = currentPosition - 1; z >= 0; z--) {
+    if (matrix.getSquare(z, row) === undefined) {
+      index = z;
+    }
+  }
+  return index;
+};
+
+export const nearestSpaceToWallRight: FindNearestSpaceToWallFunction = (
+  matrix: Matrix,
+  row: number,
+  currentPosition: number,
+): number | undefined => {
+  // start from current position + 1 to find closest empty space
+  let index: number | undefined;
+  for (let z = currentPosition + 1; z < matrix.size; z++) {
+    if (matrix.getSquare(z, row) === undefined) {
+      index = z;
+    }
+  }
+  return index;
+};
